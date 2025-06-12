3f745f84dd038be60194acd664a497d5821ac371
nagatoro <geomtrydashq2@gmail.com>
2025-06-11 21:00:24 -0300

    manda pra tutu

--- src/main/java/net/esquizo/children_of_silence_mod/ChildrenOfSilence.java
+++ src/main/java/net/esquizo/children_of_silence_mod/ChildrenOfSilence.java
@@ -1,6 +1,7 @@
 package net.esquizo.children_of_silence_mod;
 
 import com.mojang.logging.LogUtils;
+import net.esquizo.children_of_silence_mod.Init.ItemInit;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -21,6 +22,8 @@ public class ChildrenOfSilence {
 
     public ChildrenOfSilence(FMLJavaModLoadingContext context){
         IEventBus modEventBus = context.getModEventBus();
+
+        ItemInit.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
---
+++ src/main/java/net/esquizo/children_of_silence_mod/Init/ItemInit.java
@@ -0,0 +1,20 @@
+package net.esquizo.children_of_silence_mod.Init;
+
+import net.esquizo.children_of_silence_mod.ChildrenOfSilence;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ItemInit {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ChildrenOfSilence.MOD_ID);
+
+    public static final RegistryObject<Item> EXAMPLE = ITEMS.register("example",
+            () -> new Item(new Item.Properties().stacksTo(64)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
